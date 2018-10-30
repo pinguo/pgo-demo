ee379b92640079927d0e49e42f94ca3a411f81ee
zhengwei <zhengwei@camera360.com>
2018-10-30 17:26:20 +0800

    更新文档

--- src/Controller/Init.go
+++ src/Controller/Init.go
@@ -2,8 +2,11 @@ package Controller
 
 import "github.com/pinguo/pgo"
 
+// 实践中init流程最好集中放到一个init方法中，
+// 并放到包的Init.go文件中。
 func init() {
     container := pgo.App.GetContainer()
 
+    // 注册控制器到PGO容器中
     container.Bind(&WelcomeController{})
 }

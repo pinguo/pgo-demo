1036476b99449bb039c0fb4de8da412ebde66fb0
zhengwei <zhengwei@camera360.com>
2018-12-13 11:40:15 +0800

    修复Http协和泄漏的BUG

--- src/Controller/TestController.go
+++ src/Controller/TestController.go
@@ -2,9 +2,12 @@ package Controller
 
 import (
     "fmt"
+    "io/ioutil"
     "net/http"
+    "time"
 
     "github.com/pinguo/pgo"
+    "github.com/pinguo/pgo/Client/Http"
 )
 
 type TestController struct {
@@ -19,6 +22,15 @@ func (t *TestController) Construct() {
 }
 
 func (t *TestController) ActionIndex() {
+    client := t.GetObject("@pgo/Client/Http/Adapter").(*Http.Adapter)
+
+    response := client.Get("http://127.0.0.1:4000/sleep.php", nil, &Http.Option{Timeout: 2 * time.Second})
+    content, _ := ioutil.ReadAll(response.Body)
+    response.Body.Close()
+
+    fmt.Println("get response: ", string(content))
+
+
     t.str = "9999999"
     t.arr = []int{1, 2, 3}
     t.OutputJson("call /test/index, str:"+t.str, http.StatusOK)
